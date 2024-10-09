60446706fb30e45dea7766428ec4a1caab68a937
Lucas Godoy <lucasdac.godoy@gmail.com>
2024-10-09 16:27:09 -0700

    update cv/publications

--- lua-refs.lua
+++ lua-refs.lua
@@ -2,7 +2,7 @@ function Pandoc (doc)
   local references = pandoc.utils.references(doc)
   doc.blocks = pandoc.Blocks{
     pandoc.Header(1, "Publications"),
-    pandoc.Div({}, "list")
+    pandoc.Div({}, "OrderedList")
   }
   return pandoc.utils.citeproc(doc)
 end
