a4ca8574406bb647b960430acfa4b91bb531b745
Lucas Godoy <lucasdac.godoy@gmail.com>
2025-02-28 14:44:21 -0800

    updating publications page (still have one bug)

--- lua-refs.lua
+++ lua-refs.lua
@@ -2,7 +2,7 @@ function Pandoc (doc)
   local references = pandoc.utils.references(doc)
   doc.blocks = pandoc.Blocks{
     pandoc.Header(1, "Publications"),
-    pandoc.Div({}, "OrderedList")
+    pandoc.Div({}, "list")
   }
   return pandoc.utils.citeproc(doc)
 end
